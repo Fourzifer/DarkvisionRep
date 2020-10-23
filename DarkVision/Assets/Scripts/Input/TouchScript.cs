1626fa46f187f2e384dd64f3439505a58720fb10
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-10-23 02:39:01 +0200

    touch input working, with rotation. added editorconfig/omnisharp.json

--- DarkVision/Assets/Scripts/Input/TouchScript.cs
+++ DarkVision/Assets/Scripts/Input/TouchScript.cs
@@ -2,72 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine.InputSystem;
-using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
-using Mouse = UnityEngine.InputSystem.Mouse;
-using static UnityEngine.InputSystem.InputAction;
-
-public class TouchScript : MonoBehaviour
-{
+public class TouchScript : MonoBehaviour {
 
 	public Vector2 Scale = Vector2.one;
 	public Vector2 Offset = Vector2.one;
+	public GameObject SecondTrail;
 
 	[Space]
-	public Rigidbody Player;
+	public PlayerCharacterScript Player;
 	public Vector2 PlayerSpeed = Vector2.one;
+	public float PlayerRotMod = 1;
 
-	void Update()
-	{
+	private Vector2 mainTouchPosBuffer;
+	private Vector2 secondTouchPosBuffer;
+	private float angleBuffer = 0;
+
+	void Update() {
 		TouchInput();
 	}
 
-	void TouchInput()
-	{
+	void TouchInput() {
 
-		var touches = Touch.activeTouches;
-
-		if (touches.Count > 0)
-		{
-			gameObject.transform.localPosition = touches[0].screenPosition;
-			MovePlayer(touches[0].delta);
-			return;
-		}
-
-		if (Mouse.current.leftButton.isPressed)
-		{
-			gameObject.transform.localPosition = Mouse.current.position.ReadValue() * Scale - Offset;
-
-			if (!Mouse.current.leftButton.wasPressedThisFrame)
-				MovePlayer(Mouse.current.delta.ReadValue());
-
-		}
-
-		// foreach (var touch in Touch.activeTouches)
-		// Debug.Log($"{touch.touchId}: {touch.screenPosition},{touch.phase}");
-
-	}
-
-	void MovePlayer(Vector2 delta)
-	{
 		if (!Player)
-		{
+			return;
+
+		// TODO: same rotation behaviour for rotating with main finger as left finger
+		// IDEA: choose finger to use for translate depending on if angle is clockwise or ccw, and on which finger is left or right
+
+		int touchCount = Input.touchCount;
+
+		if (touchCount < 1)
+			return;
+
+		Touch touch = Input.GetTouch(0);
+
+		if (touch.phase == TouchPhase.Began) {
+			mainTouchPosBuffer = touch.position;
+		}
+
+		// Move the cube if the screen has the finger moving.
+		if (touch.phase == TouchPhase.Moved) {
+			Vector2 delta = touch.position - mainTouchPosBuffer;
+			MovePlayer(delta);
+			mainTouchPosBuffer = touch.position;
+		}
+
+		transform.localPosition = new Vector3(
+			mainTouchPosBuffer.x * Scale.x + Offset.x,
+			mainTouchPosBuffer.y * Scale.y + Offset.y,
+			transform.localPosition.z
+		);
+
+		if (Input.touchCount == 2) {
+			touch = Input.GetTouch(1);
+
+			switch (touch.phase) {
+				case TouchPhase.Began: {
+						secondTouchPosBuffer = touch.position;
+						angleBuffer = Vector2.Angle(mainTouchPosBuffer, secondTouchPosBuffer);
+					}
+					break;
+				case TouchPhase.Moved:
+				case TouchPhase.Stationary: {
+						secondTouchPosBuffer = touch.position;
+						float angle = Vector2.Angle(mainTouchPosBuffer, secondTouchPosBuffer);
+						float angleDelta = angleBuffer - angle;
+						angleBuffer = angle;
+
+						Player.Rotate(angleDelta * PlayerRotMod);
+						// Debug.Log("rotated player " + angleDelta + " degrees");
+					}
+					break;
+			}
+
+			if (SecondTrail) {
+				SecondTrail.transform.localPosition = new Vector3(
+					secondTouchPosBuffer.x * Scale.x + Offset.x,
+					secondTouchPosBuffer.y * Scale.y + Offset.y,
+					SecondTrail.transform.localPosition.z
+				);
+			}
+
+		}
+
+
+
+	}
+
+	void MovePlayer(Vector2 delta) {
+		if (!Player) {
 			return;
 		}
 
-		Player.MovePosition(Player.position + new Vector3(delta.x * PlayerSpeed.x, 0, delta.y * PlayerSpeed.y));
+		Player.MoveRelative(delta.x * PlayerSpeed.x, delta.y * PlayerSpeed.y);
 
-	}
-
-
-	public void OnFirstfinger(CallbackContext c)
-	{
-		Debug.Log("first finger: " + c.ToString());
-	}
-
-	public void OnSecondfinger(CallbackContext c)
-	{
-		Debug.Log("second finger: " + c.ToString());
 	}
 
 }
---
+++ DarkVision/Assets/Scripts/PlayerCharacterScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterScript : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	public void MoveAbsolute(float x, float z) {
+		rb.MovePosition(new Vector3(x, rb.position.y, z));
+	}
+
+	public void MoveRelative(float x, float z) {
+		var rbPos = rb.position;
+		MoveAbsolute(rbPos.x + x, rbPos.z + z);
+	}
+
+	public void Rotate(float degrees) {
+		rb.MoveRotation(rb.rotation * Quaternion.AngleAxis(degrees, Vector3.up));
+	}
+
+}
