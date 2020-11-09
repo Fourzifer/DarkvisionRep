95a352ac895c71f3a5546d58e7e9a7dff97dfaf5
Milana <milanak7797@gmail.com>
2020-11-09 14:56:51 +0100

    Auto stash before merge of "milana" and "origin/erik"

--- DarkVision/Assets/Scripts/UI/PopupHandlerScript.cs
+++ DarkVision/Assets/Scripts/UI/PopupHandlerScript.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PopupHandlerScript : MonoBehaviour {
 	private static PopupHandlerScript mainInstance;
@@ -19,8 +20,13 @@ public class PopupHandlerScript : MonoBehaviour {
 		public float Timer;
 	}
 
+	public TMP_Text Timer;
+
 	public List<PopupEntry> Popups;
 	public List<TimedPopupEntry> TimedPopups;
+
+	// IDEA: draw timer for timed popups
+	// TODO: play narrator voice for timed popups
 
 	void Start() {
 		mainInstance = this;
@@ -28,6 +34,8 @@ public class PopupHandlerScript : MonoBehaviour {
 		foreach (var popup in mainInstance.Popups) {
 			popup.Popup.SetActive(false);
 		}
+
+		Timer.gameObject.SetActive(false);
 	}
 
 	private void OnDestroy() {
@@ -35,13 +43,33 @@ public class PopupHandlerScript : MonoBehaviour {
 	}
 
 	private void Update() {
+
+		bool timerIsVisible = Timer.gameObject.activeSelf;
+		bool timerShouldBeVisible = false;
+
 		foreach (var timedPopup in mainInstance.TimedPopups) {
 			if (timedPopup.Popup.activeSelf) {
+				timerShouldBeVisible = true;
 				timedPopup.Timer -= Time.deltaTime;
-				if (timedPopup.Timer < 0)
+
+				if (timedPopup.Timer < 0) {
 					timedPopup.Popup.SetActive(false);
+					// continue;
+					break;
+				}
+
+				timerShouldBeVisible = true;
+				Timer.text = timedPopup.Timer.ToString("N2");
+				break;
 			}
 		}
+
+		if (timerIsVisible && !timerShouldBeVisible) {
+			Timer.gameObject.SetActive(false);
+		} else if (!timerIsVisible && timerShouldBeVisible) {
+			Timer.gameObject.SetActive(true);
+		}
+
 	}
 
 	public static void ShowPopup(string Key) {
@@ -55,7 +83,8 @@ public class PopupHandlerScript : MonoBehaviour {
 		}
 
 		foreach (var timedPopup in mainInstance.TimedPopups) {
-			if (timedPopup.Key == Key){
+			if (timedPopup.Key == Key) {
+				HideTimedPopups();
 				timedPopup.Popup.SetActive(true);
 				timedPopup.Timer = timedPopup.Time;
 			}
@@ -72,6 +101,18 @@ public class PopupHandlerScript : MonoBehaviour {
 		foreach (var popup in mainInstance.Popups) {
 			if (popup.Key == Key)
 				popup.Popup.SetActive(false);
+		}
+
+	}
+
+	public static void HideTimedPopups() {
+		if (mainInstance == null)
+			return;
+
+		//IDEA: reposition popups to fit all on screen without overlapping
+
+		foreach (var popup in mainInstance.TimedPopups) {
+			popup.Popup.SetActive(false);
 		}
 
 	}
