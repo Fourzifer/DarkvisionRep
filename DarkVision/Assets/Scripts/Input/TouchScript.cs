122a3d32d2f4143d2220cd4745cb4131215326b0
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-11-12 16:11:35 +0100

    fixed mouse click to interact

--- DarkVision/Assets/Scripts/Input/TouchScript.cs
+++ DarkVision/Assets/Scripts/Input/TouchScript.cs
@@ -19,17 +19,17 @@ public class TouchScript : MonoBehaviour {
 	public float PlayerRotMod = 1;
 	[Space]
 	[Tooltip("How far the finger can move when tapping to still register as an interact tap")]
-	public float TouchTapToInteractMaxDistance = 1f;
+	public float TouchTapMaxDistance = 1f;
 	[Tooltip("How much time the finger can take when holding to still register as an interact tap on release")]
-	public float TouchTapToInteractMaxTime = .1f;
+	public float TouchTapMaxTime = .1f;
 
 	[Space]
 	public bool AllowMouse = true;
 	public Vector2 MouseSpeedMod = Vector2.one;
 	[Tooltip("How far the mouse can move between clicking and releasing and still register an interact action on release")]
-	public float MouseClickToInteractMaxDistance = 1f;
+	public float MouseInteractMaxDistance = 1f;
 	[Tooltip("How much time the mouse can take between clicking and releasing and still register an interact action on release")]
-	public float MouseClickToInteractMaxTime = .1f;
+	public float MouseInteractMaxTime = .1f;
 
 	private Vector2 mainTouchPosBuffer;
 	private Vector2 secondTouchPosBuffer;
@@ -37,6 +37,7 @@ public class TouchScript : MonoBehaviour {
 	private float angleBuffer = 0;
 
 	private Vector2 mouseBuffer;
+	private bool mouseClickBuffer = false;
 	private Vector2 lastClickLocation;
 	private float mouseClickTimer;
 
@@ -48,8 +49,10 @@ public class TouchScript : MonoBehaviour {
 			clickTimer -= Time.deltaTime;
 		}
 
-		if (TouchInput())
+		if (TouchInput()) {
+
 			return;
+		}
 		if (AllowMouse)
 			MouseInput();
 	}
@@ -76,7 +79,7 @@ public class TouchScript : MonoBehaviour {
 
 		switch (touch.phase) {
 			case TouchPhase.Began:
-				clickTimer = TouchTapToInteractMaxTime;
+				clickTimer = TouchTapMaxTime;
 				mainTouchPosBuffer = touch.position;
 				lastMainTouchLocation = mainTouchPosBuffer;
 				break;
@@ -87,7 +90,7 @@ public class TouchScript : MonoBehaviour {
 				}
 				break;
 			case TouchPhase.Ended:
-				if ((touch.position - lastMainTouchLocation).sqrMagnitude < TouchTapToInteractMaxDistance * TouchTapToInteractMaxDistance
+				if ((touch.position - lastMainTouchLocation).sqrMagnitude < TouchTapMaxDistance * TouchTapMaxDistance
 				&& clickTimer > 0) {
 					Player.Interact();
 				}
@@ -145,35 +148,42 @@ public class TouchScript : MonoBehaviour {
 	void MouseInput() {
 
 		// if (!Input.GetMouseButton(0))
-			// return;
+		// return;
 
-		bool mouseDownThisFrame = Input.GetMouseButtonDown(0);
-		bool mouseUpThisFrame = Input.GetMouseButtonUp(0);
+		bool mouseDown = Input.GetMouseButton(0);
+		bool mouseDownThisFrame = mouseDown && !mouseClickBuffer; //Input.GetMouseButtonDown(0);
+		bool mouseUpThisFrame = !mouseDown && mouseClickBuffer;//Input.GetMouseButtonUp(0);
 		Vector2 currentMousePosition = Input.mousePosition;
-
-		// FIXME: mousebuffer from before last release is still used
 
 		if (mouseDownThisFrame) {
 			// Debug.Log("click");
-			clickTimer = MouseClickToInteractMaxTime;
+			clickTimer = MouseInteractMaxTime;
 			lastClickLocation = currentMousePosition;
 			mouseBuffer = currentMousePosition;
 		}
 
-		if (mouseUpThisFrame
-		&& (lastClickLocation - currentMousePosition).sqrMagnitude < MouseClickToInteractMaxDistance * MouseClickToInteractMaxDistance
-		&& clickTimer > 0) {
-			// FIXME: not working, never reached
-			Player.Interact();
+		// TODO: click and hold
+		// TODO: double tap
+		// TODO: double tap hold
+
+		if (mouseUpThisFrame) {
+
+			float releaseDelta = (lastClickLocation - currentMousePosition).sqrMagnitude;
+			float maxDistanceSquared = MouseInteractMaxDistance * MouseInteractMaxDistance;
+
+			// Debug.Log("delta on release: " + releaseDelta + " (max: " + maxDistanceSquared + "), click timer: " + clickTimer);
+
+			if (releaseDelta < maxDistanceSquared
+			&& clickTimer > 0) {
+				Player.Interact();
+			}
 		} else if (Input.GetMouseButton(0)) {
-
-
 			Vector2 delta = currentMousePosition - mouseBuffer;
 			MovePlayer(delta * MouseSpeedMod);
 		}
 
 		mouseBuffer = currentMousePosition;
-
+		mouseClickBuffer = mouseDown;
 	}
 
 	void MovePlayer(Vector2 delta) {
