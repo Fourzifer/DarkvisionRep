9b625c41795daec49ff1eb9451c3925871a8d96c
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-11-30 01:17:05 +0100

    improved npc mic listening to be able to play audio or trigger arbitrary events

--- DarkVision/Assets/Scripts/NPCListenerScript.cs
+++ DarkVision/Assets/Scripts/NPCListenerScript.cs
@@ -2,12 +2,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.Events;
 
 public class NPCListenerScript : MonoBehaviour, Utility.IObserver<(Vector3, string)> {
 
+	[Serializable]
+	public class ListenPhraseEntry {
+		public string Phrase;
+		public AudioClip Clip;
+		public UnityEvent Event;
+	}
+
 	public float HearingDistance = 15;
-	// TODO: Voices and event hook entries for each phrase
-	public string[] Phrases;
+
+	public ListenPhraseEntry[] Phrases;
 
 	void Start() {
 		Utility.Register(this, MicListenerScript.SpeakEvent);
@@ -30,9 +39,16 @@ public class NPCListenerScript : MonoBehaviour, Utility.IObserver<(Vector3, stri
 		}
 
 		string word = notification.Item2;
+		ListenPhraseEntry entry = Phrases.FirstOrDefault(firstEntry => firstEntry.Phrase == word);
 
-		if (Phrases.Contains(word)) {
+		// if (Phrases.Select(entry => entry.Phrase).Contains(word)) {
+		if (entry != null) {
 			Debug.Log("I heard you say \"" + word + "\"! I am very interested in that");
+
+			if (entry.Clip)
+				PlayerCharacterScript.PlayClip(entry.Clip);
+
+			entry.Event.Invoke();
 		} else {
 			Debug.Log("I don't know what \"" + word + "\" means");
 		}
