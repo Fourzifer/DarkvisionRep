f2619fc8de2881e9b644c48adc29ce1e6818a88d
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-12-02 00:58:22 +0100

    some player movement and npc dialogue system progress

--- DarkVision/Assets/Scripts/NPCListenerScript.cs
+++ DarkVision/Assets/Scripts/NPCListenerScript.cs
@@ -7,9 +7,13 @@ using UnityEngine.Events;
 
 public class NPCListenerScript : MonoBehaviour, Utility.IObserver<(Vector3, string)> {
 
+	// IDEA: option to define different response for keywords that are not enabled yet, hinting that they might know something yet refuse to say
+
 	[Serializable]
 	public class ListenPhraseEntry {
 		public string Phrase;
+		public bool Enabled = true;
+		public string Response;
 		public AudioClip Clip;
 		public UnityEvent Event;
 	}
@@ -18,8 +22,16 @@ public class NPCListenerScript : MonoBehaviour, Utility.IObserver<(Vector3, stri
 
 	public ListenPhraseEntry[] Phrases;
 
+	[Tooltip("What the NPC says in response to a phrase it does not recognize")]
+	public string DefaultResponse;
+	public AudioClip DefaultResponseClip;
+
+	private AudioSource narrator;
+
 	void Start() {
 		Utility.Register(this, MicListenerScript.SpeakEvent);
+
+		narrator = GetComponent<AudioSource>();
 	}
 
 	private void OnDestroy() {
@@ -39,18 +51,34 @@ public class NPCListenerScript : MonoBehaviour, Utility.IObserver<(Vector3, stri
 		}
 
 		string word = notification.Item2;
-		ListenPhraseEntry entry = Phrases.FirstOrDefault(firstEntry => firstEntry.Phrase == word);
+		ListenPhraseEntry entry = Phrases.FirstOrDefault(firstEntry => firstEntry.Enabled && firstEntry.Phrase == word);
 
 		// if (Phrases.Select(entry => entry.Phrase).Contains(word)) {
 		if (entry != null) {
-			Debug.Log("I heard you say \"" + word + "\"! I am very interested in that");
+			Debug.Log("I heard you say \"" + word + "\"! I am very interested in that. Response: " + entry.Response);
 
 			if (entry.Clip)
-				PlayerCharacterScript.PlayClip(entry.Clip);
+				narrator?.PlayOneShot(entry.Clip);
 
 			entry.Event.Invoke();
 		} else {
-			Debug.Log("I don't know what \"" + word + "\" means");
+			Debug.Log("I don't know what \"" + word + "\" means. Response: " + DefaultResponse);
+			if (DefaultResponseClip)
+				narrator?.PlayOneShot(DefaultResponseClip);
+		}
+	}
+
+	public void EnablePhrase(string phrase) {
+		ListenPhraseEntry entry = Phrases.FirstOrDefault(firstEntry => firstEntry.Phrase == phrase);
+		if (entry != null) {
+			entry.Enabled = true;
+		}
+	}
+
+	public void DisablePhrase(string phrase) {
+		ListenPhraseEntry entry = Phrases.FirstOrDefault(firstEntry => firstEntry.Phrase == phrase);
+		if (entry != null) {
+			entry.Enabled = false;
 		}
 	}
 }
