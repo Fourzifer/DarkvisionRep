cd1b7e8efc7115915336631a1890d2d1e4353b92
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-12-05 18:27:16 +0100

    added corridor-aligned forward-only movement option. player tutorial and notebook now stops when npc dialogue starts. renamed some tts files

--- DarkVision/Assets/Scripts/PlayerCharacterScript.cs
+++ DarkVision/Assets/Scripts/PlayerCharacterScript.cs
@@ -19,7 +19,8 @@ public class PlayerCharacterScript : MonoBehaviour {
 	public enum MoveMode {
 		Normal,
 		Pacman,
-		OnlyForwards
+		OnlyForwards,
+		CorridorRelativeForwardsOnly
 	}
 
 	private enum Direction {
@@ -69,6 +70,7 @@ public class PlayerCharacterScript : MonoBehaviour {
 		KeyCode.RightArrow,
 	};
 	private Direction moveDir = Direction.None;
+	private Direction facing = Direction.North;
 
 
 	[Header("Raycast footsteps")]
@@ -129,15 +131,6 @@ public class PlayerCharacterScript : MonoBehaviour {
 							lastMovementKeyPressed = key;
 					}
 
-					// if (Input.GetKeyDown(KeyCode.W))
-					// 	lastMovementKeyPressed = KeyCode.W;
-					// if (Input.GetKeyDown(KeyCode.A))
-					// 	lastMovementKeyPressed = KeyCode.A;
-					// if (Input.GetKeyDown(KeyCode.S))
-					// 	lastMovementKeyPressed = KeyCode.S;
-					// if (Input.GetKeyDown(KeyCode.D))
-					// 	lastMovementKeyPressed = KeyCode.D;
-
 					if (!Input.GetKey(lastMovementKeyPressed))
 						lastMovementKeyPressed = KeyCode.None;
 
@@ -193,6 +186,28 @@ public class PlayerCharacterScript : MonoBehaviour {
 					} else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
 						moveDir = Direction.South;
 					}
+					break;
+				case MoveMode.CorridorRelativeForwardsOnly:
+
+					switch (facing) {
+						case Direction.North:
+						case Direction.South:
+							if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+								moveDir = Direction.North;
+							} else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+								moveDir = Direction.South;
+							}
+							break;
+						case Direction.West:
+						case Direction.East:
+							if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+								moveDir = Direction.East;
+							} else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+								moveDir = Direction.West;
+							}
+							break;
+					}
+
 					break;
 			}
 
@@ -281,6 +296,15 @@ public class PlayerCharacterScript : MonoBehaviour {
 			rotateLeftPressedLastFrame = rotateLeftPressed;
 
 			targetDirection %= 360;
+			if (targetDirection < 45 || targetDirection > 315) {
+				facing = Direction.North;
+			} else if (targetDirection < 135) {
+				facing = Direction.East;
+			} else if (targetDirection < 225) {
+				facing = Direction.South;
+			} else {
+				facing = Direction.West;
+			}
 
 			// Interact
 			if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Return)) {
@@ -481,7 +505,12 @@ public class PlayerCharacterScript : MonoBehaviour {
 	}
 
 	public static void PlayClip(AudioClip clip) {
+		Narrator?.Stop();
 		Narrator?.PlayOneShot(clip);
+	}
+
+	public static void StopNarratorNow() {
+		Narrator?.Stop();
 	}
 
 
