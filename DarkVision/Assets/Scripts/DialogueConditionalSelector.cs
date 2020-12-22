752b65ac82d223961c804179fad17b2e1f249586
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-12-22 18:36:32 +0100

    small fixes

--- DarkVision/Assets/Scripts/DialogueConditionalSelector.cs
+++ DarkVision/Assets/Scripts/DialogueConditionalSelector.cs
@@ -12,13 +12,27 @@ public class DialogueConditionalSelector : MonoBehaviour {
 		GreaterOrEqualsTo
 	}
 
-	public class DialogueConditionaEntry {
+	public class DialogueConditionalEntry {
 		public string DialogueKey;
 		public Condition condition;
-
+		public int CompareValue;
 	}
+
+	public List<DialogueConditionalEntry> Entries = new List<DialogueConditionalEntry>();
 
 	public int StartingValue = 0;
 	private int value;
+
+	private void Start() {
+		value = StartingValue;
+	}
+
+	public void Set(int amount){
+		value += amount;
+	}
+
+	public void Add(int amount){
+		value = amount;
+	}
 
 }
